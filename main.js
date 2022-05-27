6bcc67f234ddb1dcd9e5e691abe20c9b664367eb
John <jbt10699@gmail.com>
2022-05-27 14:57:12 -0400

    Good amount of functionality. Some debugging needed

---
+++ main.js
@@ -0,0 +1,54 @@
+//elements
+const searchBar = document.querySelector('.search-input');
+const introText= document.querySelector('.intro_text');
+const resultBody = document.querySelector('.results_albums');
+
+
+function checkEnter(e) {
+    if (e.keyCode === 13) {
+        search()
+    }
+}
+
+
+function search() {
+    //display loading circle
+    //search, get results
+    //display numFound and serach term
+    let term = searchBar.value;
+    console.log("Searching for " + term);
+
+    fetchJsonp(`https://itunes.apple.com/search?term=${term}&media=music&entity=album&attribute=artistTerm&limit=200`)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            console.log(json.resultCount)
+            introText.innerHTML = `${json.resultCount} results for "${term}"` 
+            renderList(json.results)
+        })
+}
+
+
+function renderList(json){
+    const allAlbums = generateAllAlbums(json);
+    render(allAlbums, resultBody)
+}
+
+function generateAllAlbums(json){
+    let res = json.map(album =>{
+        return generateAlbumTmp(album);
+    }).join('');
+    return res;
+}
+
+function generateAlbumTmp(album){
+    console.log("Album: " + album.collectionName);
+    return `<div class="albums_card">
+    <img class="album_art" src=${album.artworkUrl100}>
+    <p class="album_name">${album.collectionName}</p>
+</div>`
+}
+
+function render(template, element){
+    element.innerHTML = template;
+}
