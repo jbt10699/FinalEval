657b381dda38d8715757473e1bdc4e3abbf43410
John <jbt10699@gmail.com>
2022-05-27 15:17:48 -0400

    Fixed image resizing issues

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //elements
 const searchBar = document.querySelector('.search-input');
-const introText= document.querySelector('.intro_text');
+const introText = document.querySelector('.intro_text');
 const resultBody = document.querySelector('.results_albums');
 
 
@@ -16,32 +16,45 @@ function search() {
     //search, get results
     //display numFound and serach term
     let term = searchBar.value;
-    console.log("Searching for " + term);
 
-    fetchJsonp(`https://itunes.apple.com/search?term=${term}&media=music&entity=album&attribute=artistTerm&limit=200`)
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(json);
-            console.log(json.resultCount)
-            introText.innerHTML = `${json.resultCount} results for "${term}"` 
-            renderList(json.results)
-        })
+    if (term === "") {
+        console.log("empty serach bar");
+        introText.innerHTML = "PLEASE FILL OUT THE SEARCH FIELD"
+        introText.style.color = "red"
+        searchBar.style.border = "2px solid red"
+    }
+
+    else {
+        console.log("Searching for " + term);
+        introText.style.color = null
+        searchBar.style.border = null
+
+        fetchJsonp(`https://itunes.apple.com/search?term=${term}&media=music&entity=album&attribute=artistTerm&limit=200`)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                console.log(json.resultCount)
+                introText.innerHTML = `${json.resultCount} results for "${term}"`
+                renderList(json.results)
+            })
+
+    }
 }
 
 
-function renderList(json){
+function renderList(json) {
     const allAlbums = generateAllAlbums(json);
     render(allAlbums, resultBody)
 }
 
-function generateAllAlbums(json){
-    let res = json.map(album =>{
+function generateAllAlbums(json) {
+    let res = json.map(album => {
         return generateAlbumTmp(album);
     }).join('');
     return res;
 }
 
-function generateAlbumTmp(album){
+function generateAlbumTmp(album) {
     console.log("Album: " + album.collectionName);
     return `<div class="albums_card">
     <img class="album_art" src=${album.artworkUrl100}>
@@ -49,6 +62,6 @@ function generateAlbumTmp(album){
 </div>`
 }
 
-function render(template, element){
+function render(template, element) {
     element.innerHTML = template;
 }
